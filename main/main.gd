d8b3cd58bd519c42b61a1a42877a7adec6918820
VargaDot <vancouver_cliparf@simplelogin.com>
2024-09-10 10:54:02 +0100

    Setup Composer
    Composer now caches and creates main menu and caches game scenes

--- main/main.gd
+++ main/main.gd
@@ -1,1 +1,12 @@
 extends Node
+
+func _ready() -> void:
+	ComposerGD.AddScene("MainMenu", "res://main_menu/main_menu.tscn", {
+		"instant_create":"true",
+		"scene_parent":self
+	})
+	await Composer.SceneCreated
+	
+	ComposerGD.AddScene("Game", "res://game/game.tscn", {
+		"scene_parent":self
+	})
