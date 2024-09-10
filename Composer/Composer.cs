df994eb558c32b2ca5b810f3ee0280fb195ff5b2
VargaDot <vancouver_cliparf@simplelogin.com>
2024-09-10 12:30:42 +0100

    Update Composer.cs
    Added SceneReplaced signal

--- Composer/Composer.cs
+++ Composer/Composer.cs
@@ -32,6 +32,9 @@ namespace ComposerLib
         [Signal]
         public delegate void SceneDisposedEventHandler(string sceneName);
 
+        [Signal]
+        public delegate void SceneReplacedEventHandler(string oldSceneName, string newSceneName);
+
         private readonly Loader Loader = new();
         private Godot.Collections.Dictionary<string, Scene> SceneManifest = new();
 
@@ -139,6 +142,8 @@ namespace ComposerLib
 
             RemoveScene(sceneToRemove);
             CreateScene(sceneToAdd);
+
+            EmitSignal(SignalName.SceneReplaced, sceneToRemove, sceneToAdd);
         }
 
         public async void ReloadScene(string name)
